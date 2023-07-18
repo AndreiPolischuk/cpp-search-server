df695ad0bdccc5598f82c6f7ae62d55dcd095038
Полищук Андрей Юрьевич <73225935+AndreiPolischuk@users.noreply.github.com>
2023-07-18 12:29:07 +0300

    Update search_server.cpp

--- search-server/search_server.cpp
+++ search-server/search_server.cpp
@@ -64,3 +64,75 @@ tuple<vector<string>, DocumentStatus> SearchServer::MatchDocument(const string &
   }
   return {matched_words, documents_.at(document_id).status};
 }
+
+bool SearchServer::IsStopWord(const std::string &word) const {
+    return stop_words_.count(word) > 0;
+  }
+
+static bool SearchServer::IsValidWord(const std::string &word) {
+    // A valid word must not contain special characters
+    return none_of(word.begin(), word.end(), [](char c) {
+      return c >= '\0' && c < ' ';
+    });
+  }
+
+std::vector<std::string> SearchServer::SplitIntoWordsNoStop(const std::string &text) const {
+    std::vector<std::string> words;
+    for (const std::string &word : SplitIntoWords(text)) {
+      if (!IsValidWord(word)) {
+        throw std::invalid_argument("Word " + word + " is invalid");
+      }
+      if (!IsStopWord(word)) {
+        words.push_back(word);
+      }
+    }
+    return words;
+  }
+
+static int SearchServer::ComputeAverageRating(const std::vector<int> &ratings) {
+    if (ratings.empty()) {
+      return 0;
+    }
+    int rating_sum = 0;
+    for (const int rating : ratings) {
+      rating_sum += rating;
+    }
+    return rating_sum / static_cast<int>(ratings.size());
+  }
+
+SearchServer::QueryWord SearchServer::ParseQueryWord(const std::string &text) const {
+    if (text.empty()) {
+      throw std::invalid_argument("Query word is empty");
+    }
+    std::string word = text;
+    bool is_minus = false;
+    if (word[0] == '-') {
+      is_minus = true;
+      word = word.substr(1);
+    }
+    if (word.empty() || word[0] == '-' || !IsValidWord(word)) {
+      throw std::invalid_argument("Query word " + text + " is invalid");
+    }
+
+    return {word, is_minus, IsStopWord(word)};
+  }
+
+
+SearchServer::Query SearchServer::ParseQuery(const std::string &text) const {
+    Query result;
+    for (const std::string &word : SplitIntoWords(text)) {
+      const auto query_word = ParseQueryWord(word);
+      if (!query_word.is_stop) {
+        if (query_word.is_minus) {
+          result.minus_words.insert(query_word.data);
+        } else {
+          result.plus_words.insert(query_word.data);
+        }
+      }
+    }
+    return result;
+  }
+
+double SearchServer::ComputeWordInverseDocumentFreq(const std::string &word) const {
+    return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
+  }
