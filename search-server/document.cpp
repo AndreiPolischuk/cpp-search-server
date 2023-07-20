adf13c4e75857a8caa7dadfbbeb16af0d319ef56
Полищук Андрей Юрьевич <73225935+AndreiPolischuk@users.noreply.github.com>
2023-07-20 17:56:09 +0300

    Delete search-server directory

--- search-server/document.cpp
+++
@@ -1,9 +0,0 @@
-#include "document.h"
-using namespace std;
-
-Document::Document(int id, double relevance, int rating) : id(id), relevance(relevance), rating(rating) {}
-
-std::ostream &operator<<(std::ostream &os, const Document &seg) {
-  os << "{ document_id = " << seg.id << ", relevance = " << seg.relevance << ", rating = " << seg.rating << " }";
-  return os;
-}
